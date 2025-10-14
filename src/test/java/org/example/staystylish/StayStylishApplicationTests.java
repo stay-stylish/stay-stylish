aa2c2f5f30c0894100ce792a6af36da528741c75
youngrae0317 <isp1229@naver.com>
2025-10-14 12:17:06 +0900

    feat: 초기 프로젝트 세팅

---
+++ src/test/java/org/example/staystylish/StayStylishApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.staystylish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayStylishApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
