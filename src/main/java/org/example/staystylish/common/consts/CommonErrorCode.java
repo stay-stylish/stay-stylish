8bd66f632edc0dd6e1a34d96505c00b3a8485b59
youngrae0317 <145316807+youngrae0317@users.noreply.github.com>
2025-10-15 17:48:04 +0900

    refactor: 전역 예외 처리 로직 리팩토링 (#6)
    * refactor: 전역 예외 처리 로직 리팩토링
    
    * refactor: 지원하지 않은 HTTP 메서드 예외 처리 부분 추가

--- src/main/java/org/example/staystylish/common/consts/CommonErrorCode.java
+++ src/main/java/org/example/staystylish/common/consts/CommonErrorCode.java
@@ -8,10 +8,13 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum CommonErrorCode implements ErrorCode {
 
-    // 예시입니다.
-    REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 후기입니다.");
+    // 공통 오류
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "유효하지 않은 입력값입니다."),
+    METHOD_NOT_ALLOWED(HttpStatus.METHOD_NOT_ALLOWED, "지원하지 않는 HTTP Method 입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다."),
+    INVALID_TYPE_VALUE(HttpStatus.BAD_REQUEST, "입력값의 타입이 올바르지 않습니다."),
+    MISSING_REQUEST_PARAMETER(HttpStatus.BAD_REQUEST, "필수 요청 파라미터가 누락되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
 }
-
