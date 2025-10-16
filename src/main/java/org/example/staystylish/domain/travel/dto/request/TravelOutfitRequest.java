c88f81e196254fc846ec247ab57b5515bc620109
youngrae0317 <145316807+youngrae0317@users.noreply.github.com>
2025-10-16 20:56:40 +0900

    feat: Travel 도메인 클래스 생성 (#18)
    * feat: Travel 도메인 클래스 생성
    
    * refactor: Builder 수정 및 컨벤션 적용"

---
+++ src/main/java/org/example/staystylish/domain/travel/dto/request/TravelOutfitRequest.java
@@ -0,0 +1,13 @@
+package org.example.staystylish.domain.travel.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+public record TravelOutfitRequest(
+        @NotBlank String country,
+        @NotBlank String city,
+        @NotNull LocalDate startDate,
+        @NotNull LocalDate endDate
+) {
+}
