066b6fa2dc27f3e8ba320d454ce7664d66443e62
jihoLee0818 <leejiho0114@gmail.com>
2025-10-27 09:13:28 +0900

    feat: gemini 관련 설정 삭제 (#67)
    * feat: 상품 분류 후 db에 저장하는 로직 추가
    
    * feat: gemini관련 로직 openAI 사용으로 변경

--- src/main/java/org/example/staystylish/common/config/AiClientsConfig.java
+++ src/main/java/org/example/staystylish/common/config/AiClientsConfig.java
@@ -5,7 +5,6 @@ import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 /**
  * - chatClientGemini (@Primary): 기본 (Gemini) / chatClientOpenAi : traveloutfit 도메인에서 사용 (OpenAI)
@@ -13,22 +12,7 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class AiClientsConfig {
 
-    // 기본 ChatModel을 Gemini로 고정
-    @Bean
-    @Primary
-    public ChatModel primaryChatModel(@Qualifier("vertexAiGeminiChat") ChatModel gemini) {
-
-        return gemini;
-    }
-
-    // 기본 ChatClient (Gemini)
-    @Bean(name = "chatClientGemini")
-    @Primary
-    public ChatClient chatClientGemini(@Qualifier("vertexAiGeminiChat") ChatModel geminiModel) {
-        return ChatClient.builder(geminiModel).build();
-    }
-
-    // traveloutfit 도메인 chatClient (OpenAI)
+    //chatClient (OpenAI) - 이제 이 빈이 기본이 됨
     @Bean(name = "chatClientOpenAi")
     public ChatClient chatClientOpenAi(@Qualifier("openAiChatModel") ChatModel openAiModel) {
         return ChatClient.builder(openAiModel).build();
