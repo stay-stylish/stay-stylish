65fdc7919b75872c986da6628cf38102c1236b86
youngrae0317 <145316807+youngrae0317@users.noreply.github.com>
2025-11-05 11:24:17 +0900

    feat: 해외 여행 옷차림 추천 기능 Retry 적용 (#113)

--- src/main/java/org/example/staystylish/domain/traveloutfit/ai/TravelAiClient.java
+++ src/main/java/org/example/staystylish/domain/traveloutfit/ai/TravelAiClient.java
@@ -2,6 +2,7 @@ package org.example.staystylish.domain.traveloutfit.ai;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.example.staystylish.common.exception.GlobalException;
 import org.example.staystylish.domain.traveloutfit.code.TravelOutfitErrorCode;
@@ -32,6 +33,7 @@ public class TravelAiClient {
 
     // chatClient를 사용해서 프롬프트를 AI 모델로 전송하고 응답 받기
     @Cacheable(value = "travelAi", key = "#prompt")
+    @Retry(name = "travelAiApi", fallbackMethod = "fallbackCallForJson")
     @CircuitBreaker(name = "travelAiApi", fallbackMethod = "fallbackCallForJson")
     public String callForJson(String prompt) {
 
